df3968313f742f084eb9950a7522a2e4534e69a8
matiev <matiev@live.com>
2026-01-14 19:31:42 +0300

    edit

--- app.ts
+++ app.ts
@@ -6,7 +6,6 @@ console.log(arr01); //(2) [22, 'hello'];
 const arr02 : Array<string | number> = [22, 'hi'];
 console.log(arr02); //(2) [22, 'hello'];
 
-
 // const cort01 : [string, boolean] = ['hello', false];
 // console.log(cort01); //(2) ['hello', false]
 
