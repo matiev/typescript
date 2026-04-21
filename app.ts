7bee1f85ce7a28b48274714893369f7df131d91c
matiev <matiev@live.com>
2026-04-21 14:45:01 +0300

    edit

--- app.ts
+++ app.ts
@@ -1,7 +1,6 @@
 const arr01 : (string | number)[] = [22, 'hello'];
 console.log(arr01); //(2) [22, 'hello'];
 
-
 const arr02 : Array<string | number> = [22, 'hi'];
 console.log(arr02); //(2) [22, 'hello'];
 
